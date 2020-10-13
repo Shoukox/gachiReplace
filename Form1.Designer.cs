cc1746b83d5bea9255c6c3d8d361fc77beba66c5
Shoukox <58671746+Shoukox@users.noreply.github.com>
2020-10-13 19:09:33 +0500

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace gasm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxInput = new System.Windows.Forms.TextBox();
+            this.textBoxOutput = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.MinimalLetters = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxStatus = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxChance = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBoxInput
+            // 
+            this.textBoxInput.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.textBoxInput.Location = new System.Drawing.Point(12, 26);
+            this.textBoxInput.Multiline = true;
+            this.textBoxInput.Name = "textBoxInput";
+            this.textBoxInput.Size = new System.Drawing.Size(398, 177);
+            this.textBoxInput.TabIndex = 0;
+            // 
+            // textBoxOutput
+            // 
+            this.textBoxOutput.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold);
+            this.textBoxOutput.Location = new System.Drawing.Point(416, 26);
+            this.textBoxOutput.Multiline = true;
+            this.textBoxOutput.Name = "textBoxOutput";
+            this.textBoxOutput.Size = new System.Drawing.Size(384, 177);
+            this.textBoxOutput.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Segoe UI Semibold", 15F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.button1.Location = new System.Drawing.Point(680, 209);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 40);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "go))0";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // MinimalLetters
+            // 
+            this.MinimalLetters.Location = new System.Drawing.Point(162, 220);
+            this.MinimalLetters.Name = "MinimalLetters";
+            this.MinimalLetters.Size = new System.Drawing.Size(30, 22);
+            this.MinimalLetters.TabIndex = 3;
+            this.MinimalLetters.Text = "4";
+            this.MinimalLetters.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 216);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(144, 26);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Минимальное количество\r\n букв в слове для замены";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI Semibold", 15F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.label2.Location = new System.Drawing.Point(12, -3);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 28);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Input";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI Semibold", 15F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.label3.Location = new System.Drawing.Point(416, -3);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(76, 28);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Output";
+            // 
+            // textBoxStatus
+            // 
+            this.textBoxStatus.Font = new System.Drawing.Font("Segoe UI Semibold", 10F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.textBoxStatus.Location = new System.Drawing.Point(560, 209);
+            this.textBoxStatus.Multiline = true;
+            this.textBoxStatus.Name = "textBoxStatus";
+            this.textBoxStatus.Size = new System.Drawing.Size(114, 40);
+            this.textBoxStatus.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(357, 216);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(76, 26);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Шанс замены\r\n(без %)";
+            // 
+            // textBoxChance
+            // 
+            this.textBoxChance.Location = new System.Drawing.Point(439, 220);
+            this.textBoxChance.Name = "textBoxChance";
+            this.textBoxChance.Size = new System.Drawing.Size(32, 22);
+            this.textBoxChance.TabIndex = 9;
+            this.textBoxChance.Text = "50";
+            this.textBoxChance.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(812, 254);
+            this.Controls.Add(this.textBoxChance);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBoxStatus);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.MinimalLetters);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBoxOutput);
+            this.Controls.Add(this.textBoxInput);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxInput;
+        private System.Windows.Forms.TextBox textBoxOutput;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox MinimalLetters;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxStatus;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxChance;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace gasm
+{
+    public partial class Form1 : Form
+    {
+        string[] gachi = new string[]
+        {
+            ":male_sign:AAAAHHHH$:male_sign:",
+":male_sign:Fisting is 300$:male_sign:",
+":male_sign:Fucking slaves:male_sign:",
+":male_sign:AhhAhhAAAAHHH:male_sign:",
+":male_sign:Fucking cumming:male_sign:",
+":male_sign:boy next door:male_sign:",
+":male_sign:deep dark fantasies:male_sign:",
+":male_sign:boss in this gym:male_sign:",
+":male_sign:semen:male_sign:",
+":male_sign:Wooo:male_sign:",
+":male_sign:What the hell are you two doing:male_sign:",
+":male_sign:oh shit i'm sorry:male_sign:",
+":male_sign:dungeon master:male_sign:",
+":male_sign:that's amazing:male_sign:",
+        }.Select(m => m.Replace(":male_sign:", "♂")).ToArray();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string replaceToGachi(string text)
+        {
+            List<int> rndNumbers = new List<int>();
+            Random random = new Random();
+            for (int i = 0; i <= text.Length - 1; i++)
+            {
+                rndNumbers.Add(random.Next(0, 101));
+            }
+            try
+            {
+                string[] ab = textBoxInput.Text.Split(' ');
+                for (int i = 0; i <= ab.Length - 1; i++)
+                {
+                    if (rndNumbers[i] <= int.Parse(textBoxChance.Text))
+                        if (ab[i].Length >= int.Parse(MinimalLetters.Text)) ab[i] = gachi[random.Next(0, gachi.Length)];
+                }
+                textBoxStatus.Text = $"Букв: {MinimalLetters.Text}\r\nШанс: {textBoxChance.Text}%";
+                return string.Join(" ", ab);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+            return null;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBoxOutput.Text = replaceToGachi(textBoxInput.Text);
+        }
+    }
+}
